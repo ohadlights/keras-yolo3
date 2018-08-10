36c18fd7a37618958926265dc4a2c3b359b5af66
omeir1 <ohad.meir@intel.com>
2018-08-10 19:02:57 +0300

    train.py - epoch is half real epoch (start with that... see that all goes well...

--- train.py
+++ train.py
@@ -62,7 +62,7 @@ def _main(args):
 
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes),
-                            steps_per_epoch=max(1, num_train//batch_size),
+                            steps_per_epoch=max(1, num_train//batch_size) // 2,
                             validation_data=data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes),
                             validation_steps=max(1, num_val//batch_size),
                             epochs=50,
