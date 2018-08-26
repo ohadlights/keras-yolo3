8469f97828f7f798c708bb95e6da3e5edb4026b2
omeir1 <ohad.meir@intel.com>
2018-08-26 11:06:30 +0300

    handle 16 bit?

--- yolo3/utils.py
+++ yolo3/utils.py
@@ -92,7 +92,8 @@ def get_random_data(annotation_line, input_shape, random=True, max_boxes=20, jit
     hue = rand(-hue, hue)
     sat = rand(1, sat) if rand()<.5 else 1/rand(1, sat)
     val = rand(1, val) if rand()<.5 else 1/rand(1, val)
-    x = rgb_to_hsv(np.array(image)/255.)
+    scale = 255. if len(image.shape) == 3 else 65535.
+    x = rgb_to_hsv(np.array(image)/scale)
     x[..., 0] += hue
     x[..., 0][x[..., 0]>1] -= 1
     x[..., 0][x[..., 0]<0] += 1
